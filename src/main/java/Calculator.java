fd08ed1c2f8d53784fb7e1b297d97f98086e09cd
Anastasiya Sudarenko <snastiya@mail.ru>
2017-10-11 20:40:34 +0300

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * Created by ASudarenko on 08.10.2017.
+ * @author ASudarenko
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+/* Ввод переменных 1 */
+        System.out.println("Введите первое число: ");
+        float first = scanner.nextFloat();
+        System.out.println("Введите второе число: ");
+        float second = scanner.nextFloat();
+        float sum;
+        sum = first + second;
+        System.out.printf("%.4f", sum);
+    }
+
+}
